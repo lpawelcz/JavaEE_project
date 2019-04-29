b6c6f6ed18341a3603cac5f782d43c340d1b4c83
lpawelcz <czarnecki.l.p@gmail.com>
2019-04-29 10:08:47 +0200

    User annotations - mapping

--- src/Database/User.java
+++ src/Database/User.java
@@ -5,8 +5,12 @@ import javax.persistence.*;
 @Table(name = "User")
 public class User {
 	
+	@Id
+	@Column(name = "userID")
 	private int userID;
+	@Column(name = "name")
 	private String name;
+	@Column(name = "password")
 	private String password;
 
 //	public User(String name, String password) {
@@ -15,9 +19,6 @@ public class User {
 //		this.password = password;
 //	}
 
-	@Id
-	@Column(name = "userID")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	public int getuserID(){
 		return userID;
 	}
