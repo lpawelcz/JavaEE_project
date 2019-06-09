0356104baec2ced5ce74e0d488627b8a463ca72b
lpawelcz <czarnecki.l.p@gmail.com>
2019-06-09 22:02:20 +0200

    Sprzątanko servleta

--- src/pl/javastart/servlets/SelectedTestSend.java
+++ src/pl/javastart/servlets/SelectedTestSend.java
@@ -12,50 +12,27 @@ import javax.servlet.http.HttpServletResponse;
 import database.entities.Test;	//import wszystkich klas z bazy danych
 import Manage.ManageTest;
 
-/**
- * Servlet implementation class SelectedTestSend
- */
+
 @WebServlet("/SelectedTestSend")
 public class SelectedTestSend extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ManageTest manageTests = new ManageTest();  
 	public Test test;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public SelectedTestSend() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		//request.setAttribute("test_id", 1);
-		//request.getRequestDispatcher("Bashownik/SelectedTest.jsp").forward(request, response);
-		
-		//PrintWriter out = response.getWriter();
+        String test_ID = request.getParameter("test_ID");								// pobierz z requesta id testu
+        test = manageTests.GetTest(Integer.parseInt(test_ID));							// pobierz test o danym id
+        
+        request.setAttribute("Test", test);												// zapisz obiekt testu w sesji
 
-        String test_ID = request.getParameter("test_ID");
-        test = manageTests.GetTest(Integer.parseInt(test_ID));
-        //out.println("ID wybranego testu:");
-        //out.println(test_ID);
-        
-        request.setAttribute("Test", test);
-
-        // Forward to to the JSP file.
-        request.getRequestDispatcher("SelectedTest.jsp").forward(request, response);
-        
-        
+        request.getRequestDispatcher("SelectedTest.jsp").forward(request, response);	// przekieruj sterowanie do jsp        
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
